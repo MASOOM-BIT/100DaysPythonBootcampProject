fd0ae6c44de0e4153cfe4d52afa21710ceeb5902
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-01-31 03:29:41 +0530

    starting turtle

---
+++ DAY16/Program1.py
@@ -0,0 +1,12 @@
+import turtle
+turtle_obj1 = turtle.Turtle()
+print(turtle_obj1)
+turtle_obj1.shape("turtle")
+turtle_obj1.color('DodgerBlue3')
+turtle_obj1.forward(100)
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
+
