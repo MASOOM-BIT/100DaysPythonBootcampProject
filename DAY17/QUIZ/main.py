8106d1798b56f82c635825dc3dfef0a394314927
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-02-26 01:53:05 +0530

    DAY 17 Quiz

--- DAY17/QUIZ/main.py
+++ DAY17/QUIZ/main.py
@@ -1,11 +1,17 @@
 from question_model import Questions
 from data import question_data
+from quiz_brain import  QuizBrain
 
 question_bank = []
 for questions in question_data:
-    question_text = questions['text']
-    question_answer = questions['answer']
+    question_text = questions['question']
+    question_answer = questions['correct_answer']
     new_question = Questions(q_text=question_text,q_answer=question_answer)
     question_bank.append(new_question)
 
-print(question_bank[0].answer)
+quiz=QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You have completed the Quiz")
+print(f'Your funal score was {quiz.score}/{quiz.question_number}')
